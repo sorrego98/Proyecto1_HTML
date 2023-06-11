f3c54de9a2a006155411659e10972e6bf9d75fcd
sorrego98 <juan.sorrego98@gmail.com>
2023-06-11 15:33:27 -0500

    Creación del espacio de trabajo

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const express = require(`express`);
+const path = require(`path`);
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, `../public`)));
+app.listen(3030, () => console.log(`Server Done`));
